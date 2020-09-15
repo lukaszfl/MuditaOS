6e4121e195dce15eae4fbb356a690cf32fed0b7a
Bartek Cichocki <bartosz.cichocki@mudita.com>
2020-09-15 08:47:17 +0200

    a2dp test

--- module-apps/application-settings/windows/BtWindow.cpp
+++ module-apps/application-settings/windows/BtWindow.cpp
@@ -89,9 +89,21 @@ namespace gui
             return true;
         });
 
-        add_box_label(box, "  -> Visibility", [=](Item &) {
-            LOG_DEBUG("Callback visibility");
+        add_box_label(box, "  -> Set visible", [=](Item &) {
+            LOG_DEBUG("Callback set visibility");
             message_bt(application, BluetoothMessage::Request::Visible);
+            return true;
+        });
+
+        add_box_label(box, "  -> Play", [=](Item &) {
+            LOG_DEBUG("Start playback");
+            message_bt(application, BluetoothMessage::Request::Play);
+            return true;
+        });
+
+        add_box_label(box, "  -> Stop", [=](Item &) {
+            LOG_DEBUG("Stop playback");
+            message_bt(application, BluetoothMessage::Request::Stop);
             return true;
         });
 
